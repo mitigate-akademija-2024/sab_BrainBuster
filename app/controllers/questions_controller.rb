d41225b50f39735c053ea8159943e1ed6b35ebcc
xsabx <113295583+xsabx@users.noreply.github.com>
2024-08-09 12:50:08 +0000

    Question delete and edit function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_quiz, only: [:new, :create]
+  before_action :set_question, only: [:destroy, :edit, :update]
 
   def index
   end
@@ -29,13 +30,35 @@ class QuestionsController < ApplicationController
     render :new
   end
 
+  
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to quiz_url(@question.quiz), notice: "Question was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @question.destroy!
+
+    redirect_to quiz_path(@question.quiz), notice: "Quiz was successfully destroyed."
+    end
+  end
+
   private
 
   def set_quiz
     @quiz = Quiz.find(params[:quiz_id])
   end
 
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
   def question_params
     params.require(:question).permit(:question_text, answers_attributes: [:id, :answer_text, :correct])
   end
-end
