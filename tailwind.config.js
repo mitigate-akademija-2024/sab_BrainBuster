8442a7b901edb5ce71fb806fa1f027a72e98c23c
xsabx <113295583+xsabx@users.noreply.github.com>
2024-08-13 09:31:18 +0000

    a bit of interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'light-blue': '#676f9d',
+        'medium-blue': '#424769',
+        'dark-blue': '#2d3250',
+        'orange': '#f9b17a',
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
